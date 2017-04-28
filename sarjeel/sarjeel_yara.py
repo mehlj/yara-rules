95ef45ba4a61948f3109aacc30fd337b7add277d
Justen Mehl <justenmehl12@gmail.com>
2017-04-28 17:31:58 -0400

    Update sarjeel_yara.py
    Added CLI argument for file specification

--- sarjeel/sarjeel_yara.py
+++ sarjeel/sarjeel_yara.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
-import yara
 
-def match_sarjeel():    
+import yara
+import getopt
+import sys
+
+
+def match_sarjeel(file):    
     rule_str = """rule sarjeel
     {
     meta:
@@ -17,9 +21,30 @@ def match_sarjeel():
     """
 
     rule = yara.compile(source=rule_str)
-    matches = rule.match('/home/jmehl/samples/sarjeel.exe')
+    matches = rule.match(file)
 
     return matches
 
+
+def get_args(argv):
+    try:
+        opts, args = getopt.getopt(argv, "hf:", ["file="])
+    except getopt.GetoptError:
+        print('./sarjeel_yara.py -f <file>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('./sarjeel_yara.py -f <file>')
+            sys.exit()
+        elif opt in ("-f", "--file"):
+            target = arg
+            return target
+        else:
+            print('./sarjell_yara.py -f <file')
+            sys.exit()
+
+
 if __name__ == "__main__":
-    print(match_sarjeel())
+    file = get_args(sys.argv[1:])
+    print(match_sarjeel(file))
+
